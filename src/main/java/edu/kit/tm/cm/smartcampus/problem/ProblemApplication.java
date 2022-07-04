74645f847a9a65ced00853e472c13fbbde7b7730
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-07-04 15:50:49 +0200

    imports gefixed

--- src/main/java/edu/kit/tm/cm/smartcampus/problem/ProblemApplication.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/problem/ProblemApplication.java
@@ -1,4 +1,4 @@
-package main.java.edu.kit.tm.cm.smartcampus.problem;
+package edu.kit.tm.cm.smartcampus.problem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
