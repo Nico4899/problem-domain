63c3fa5d3057fc5cbbbf3cad3f9e261a1c7a636e
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-07-19 15:39:19 +0200

    cosmetics

--- src/main/java/edu/kit/tm/cm/smartcampus/problem/logic/model/Problem.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/problem/logic/model/Problem.java
@@ -15,39 +15,40 @@ import java.sql.Timestamp;
 @Entity(name = "problem")
 public class Problem {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "problem_sequence")
-  @SequenceGenerator(name = "problem_sequence", allocationSize = 1)
-  @GenericGenerator(
-      name = "problem_sequence",
-      strategy =
-          "edu/kit/tm/cm/smartcampus/building/infrastructure/database/PrefixSequenceGenerator.java",
-      parameters = {
-        @org.hibernate.annotations.Parameter(
-            name = PrefixSequenceGenerator.VALUE_PREFIX_PARAMETER,
-            value = "p-")
-      })
-  @Column(name = "identification_number")
-  private String identificationNumber;
-
-  @Column(name = "notification_identification_number")
-  private String notificationIdentificationNumber;
-
-  @Column(name = "priblem_title")
+  @Column(name = "problem_title")
   private String problemTitle;
 
   @Column(name = "problem_description")
   private String problemDescription;
 
-  @Column(name = "creation_time")
-  private Timestamp creationTime;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "problem_sequence")
+  @SequenceGenerator(name = "problem_sequence", allocationSize = 1)
+  @GenericGenerator(
+          name = "problem_sequence",
+          strategy =
+                  "edu/kit/tm/cm/smartcampus/building/infrastructure/database/PrefixSequenceGenerator.java",
+          parameters = {
+                  @org.hibernate.annotations.Parameter(
+                          name = PrefixSequenceGenerator.VALUE_PREFIX_PARAMETER,
+                          value = "p-")
+          })
+  @Column(name = "identification_number")
+  private String identificationNumber;
 
-  @Column(name = "problem_reporter")
-  private String problemReporter;
+  @Column(name = "reference_identification_number")
+  private String referenceIdentificationNumber;
+
+  @Column(name = "notification_identification_number")
+  private String notificationIdentificationNumber;
 
   @Column(name = "problem_state")
   private ProblemState problemState;
 
-  @Column(name = "reference_identification_number")
-  private String referenceIdentificationNumber;
+  @Column(name = "problem_reporter")
+  private String problemReporter;
+
+  @Column(name = "creation_time")
+  private Timestamp creationTime;
+
 }
