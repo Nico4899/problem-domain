682496596136b160088bac1b37abe3f3ae2ecee2
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-07-20 13:18:16 +0200

    added ProblemInputValidator and call him in ProblemService

--- src/main/java/edu/kit/tm/cm/smartcampus/problem/infrastructure/service/ProblemService.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/problem/infrastructure/service/ProblemService.java
@@ -4,19 +4,17 @@ package edu.kit.tm.cm.smartcampus.problem.infrastructure.service;
 import edu.kit.tm.cm.smartcampus.problem.infrastructure.database.ProblemRepository;
 import edu.kit.tm.cm.smartcampus.problem.infrastructure.exceptions.InvalidArgumentsException;
 import edu.kit.tm.cm.smartcampus.problem.infrastructure.exceptions.ResourceNotFoundException;
-import edu.kit.tm.cm.smartcampus.problem.infrastructure.validator.InputValidator;
+import edu.kit.tm.cm.smartcampus.problem.infrastructure.validator.ProblemInputValidator;
 import edu.kit.tm.cm.smartcampus.problem.infrastructure.validator.ServiceValidator;
 import edu.kit.tm.cm.smartcampus.problem.logic.model.Problem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
 
-import static edu.kit.tm.cm.smartcampus.problem.utils.*;
+import static edu.kit.tm.cm.smartcampus.problem.utils.NOT_FOUND;
 
 @Service
 public class ProblemService {
@@ -28,15 +26,15 @@ public class ProblemService {
 
   private final ProblemRepository problemRepository;
 
-  private final InputValidator inputValidator;
+  private final ProblemInputValidator problemInputValidator;
 
   private final ServiceValidator serviceValidator;
 
   @Autowired
-  public ProblemService(ProblemRepository problemRepository, InputValidator inputValidator,
+  public ProblemService(ProblemRepository problemRepository, ProblemInputValidator problemInputValidator,
                         ServiceValidator serviceValidator) {
     this.problemRepository = problemRepository;
-    this.inputValidator = inputValidator;
+    this.problemInputValidator = problemInputValidator;
     this.serviceValidator = serviceValidator;
   }
 
@@ -47,7 +45,7 @@ public class ProblemService {
   }
 
   public Problem getProblem(String pin) throws ResourceNotFoundException, InvalidArgumentsException {
-    validatePin(pin);
+    problemInputValidator.validatePin(pin);
     serviceValidator.validateInIsMapped(problemRepository, pin);
     Optional<Problem> optionalProblem = problemRepository.findById(pin);
     if (optionalProblem.isEmpty()) {
@@ -57,60 +55,21 @@ public class ProblemService {
   }
 
   public Problem createProblem(Problem problem) {
-    validateProblem(problem); //TODO hier evtl noch nicht die id prüfen
-    serviceValidator.validateInDoesNotExist(problemRepository, problem.getPin());//TODO @Johannes auch noch referenceId usw pruefen?
+    problemInputValidator.validateProblem(problem);
+    serviceValidator.validateInDoesNotExist(problemRepository, problem.getPin());
     return problemRepository.save(problem);
   }
 
   public Problem updateProblem(Problem problem) {
-    validateProblem(problem);
+    problemInputValidator.validateProblem(problem);
     serviceValidator.validateInIsMapped(problemRepository, problem.getPin());
     return problemRepository.save(problem);
   }
 
   public void removeProblem(String pin) {
-    validatePin(pin);
+    problemInputValidator.validatePin(pin);
     serviceValidator.validateInIsMapped(problemRepository, pin);
     problemRepository.deleteById(pin);
-  }
-
-  /**
-   * Validates a given Problem (regarding everything that has nothing to do with the database) using the InputValidator.
-   *
-   * @param problem the problem to be validated
-   */
-  private void validateProblem(Problem problem) { //TODO validate creation time
-    inputValidator.validateNotNull(Map.of(
-            "problem title", problem.getProblemTitle(),
-            "problem description", problem.getProblemDescription(),
-            "problem identification number", problem.getPin(),
-            "problem reference identification number", problem.getReferenceIn(),
-            "problem notification identification number", problem.getNin(),
-            "problem state", problem.getProblemState(),
-            "problem reporter", problem.getProblemReporter(),
-            "problem creation time attribute", problem.getCreationTime(),
-            "problem creation time", problem.getCreationTime().getTime()));
-
-    inputValidator.validateNotEmpty(Map.of(
-            "problem title", problem.getProblemTitle(),
-            "problem description", problem.getProblemDescription(),
-            "problem reporter", problem.getProblemReporter()));
-
-    inputValidator.validateMatchesRegex(Map.of(
-            "problem identification number", Pair.of(problem.getPin(), PIN_PATTERN),
-            "problem reference identification number", Pair.of(problem.getReferenceIn(), REFERENCE_IN_PATTERN),
-            "problem notification identification number",
-            Pair.of(problem.getNin(), NIN_PATTERN)));
-  }
-
-  /**
-   * Validates a given pin (regarding everything that has nothing to do with the database) using the InputValidator.
-   *
-   * @param pin the pin to be validated
-   */
-  private void validatePin(String pin) {
-    inputValidator.validateNotNull(Map.of("pin", pin));
-    inputValidator.validateMatchesRegex(Map.of("pin", Pair.of(pin, PIN_PATTERN)));
   }
 
 }
---
+++ src/main/java/edu/kit/tm/cm/smartcampus/problem/infrastructure/validator/ProblemInputValidator.java
@@ -0,0 +1,54 @@
+package edu.kit.tm.cm.smartcampus.problem.infrastructure.validator;
+
+import edu.kit.tm.cm.smartcampus.problem.logic.model.Problem;
+import org.springframework.data.util.Pair;
+
+import java.util.Map;
+
+/**
+ * Class representing a problem input validator which checks given objects from the problem model and thereby
+ * validates them and throws the right exceptions when an input is invalid.
+ */
+public class ProblemInputValidator extends InputValidator {
+
+  /**
+   * Validates a given Problem (regarding everything that has nothing to do with the database) using the InputValidator
+   *
+   * @param problem the problem to be validated
+   */
+  public void validateProblem(Problem problem) { //TODO validate creation time
+    validateNotNull(Map.of(
+            "problem", problem,
+            "problem title", problem.getProblemTitle(),
+            "problem description", problem.getProblemDescription(),
+            "problem identification number", problem.getPin(),
+            "problem reference identification number", problem.getReferenceIn(),
+            "problem notification identification number", problem.getNin(),
+            "problem state", problem.getProblemState(),
+            "problem reporter", problem.getProblemReporter(),
+            "problem creation time attribute", problem.getCreationTime(),
+            "problem creation time", problem.getCreationTime().getTime()));
+
+    validateNotEmpty(Map.of(
+            "problem title", problem.getProblemTitle(),
+            "problem description", problem.getProblemDescription(),
+            "problem reporter", problem.getProblemReporter()));
+
+    validateMatchesRegex(Map.of(
+            "problem identification number", Pair.of(problem.getPin(), "PIN_PATTERN"),//TODO
+            "problem reference identification number", Pair.of(problem.getReferenceIn(), "REFERENCE_IN_PATTERN"),
+            "problem notification identification number",
+            Pair.of(problem.getNin(), "NIN_PATTERN")));
+  }
+
+  /**
+   * Validates a given pin (regarding everything that has nothing to do with the database) using the InputValidator
+   *
+   * @param pin the pin to be validated
+   */
+  public void validatePin(String pin) {
+    validateNotNull(Map.of("pin", pin));
+    validateMatchesRegex(Map.of("pin", Pair.of(pin, "PIN_PATTERN")));
+  }
+
+}
