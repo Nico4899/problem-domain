80c488f06162e9d3d1b15e29b1d79ebfa5800962
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-07-25 10:58:02 +0200

    cosmetics again

--- src/main/java/edu/kit/tm/cm/smartcampus/problem/logic/model/Problem.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/problem/logic/model/Problem.java
@@ -21,19 +21,21 @@ import static edu.kit.tm.cm.smartcampus.problem.logic.model.Problem.PROBLEM_TABL
 @Entity(name = PROBLEM_TABLE_NAME)
 public class Problem {
 
-  /** The constant PROBLEM_TABLE_NAME. */
+  /**
+   * The constant PROBLEM_TABLE_NAME.
+   */
   // table name (must be public, else annotation can't find it)
   public static final String PROBLEM_TABLE_NAME = "problem";
 
   // constants this class uses
   private static final String REFERENCE_IDENTIFICATION_NUMBER_COLUMN =
-      "reference_identification_number";
+          "reference_identification_number";
   private static final String NOTIFICATION_IDENTIFICATION_NUMBER_COLUMN =
-      "notification_identification_number";
+          "notification_identification_number";
   private static final String CREATION_TIME_COLUMN = "creation_time";
   private static final String PROBLEM_SEQUENCE_NAME = "problem_sequence";
   private static final String GENERATOR_PATH =
-      "edu.kit.tm.cm.smartcampus.problem.infrastructure.database.PrefixSequenceGenerator";
+          "edu.kit.tm.cm.smartcampus.problem.infrastructure.database.PrefixSequenceGenerator";
   private static final String PROBLEM_IDENTIFICATION_NUMBER_PREFIX = "p-";
   private static final String IDENTIFICATION_NUMBER_COLUMN = "identification_number";
 
@@ -41,13 +43,13 @@ public class Problem {
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = PROBLEM_SEQUENCE_NAME)
   @SequenceGenerator(name = PROBLEM_SEQUENCE_NAME, allocationSize = 1)
   @GenericGenerator(
-      name = PROBLEM_SEQUENCE_NAME,
-      strategy = GENERATOR_PATH,
-      parameters = {
-        @Parameter(
-            name = PrefixSequenceGenerator.VALUE_PREFIX_PARAMETER,
-            value = PROBLEM_IDENTIFICATION_NUMBER_PREFIX)
-      })
+          name = PROBLEM_SEQUENCE_NAME,
+          strategy = GENERATOR_PATH,
+          parameters = {
+                  @Parameter(
+                          name = PrefixSequenceGenerator.VALUE_PREFIX_PARAMETER,
+                          value = PROBLEM_IDENTIFICATION_NUMBER_PREFIX)
+          })
   @Column(name = IDENTIFICATION_NUMBER_COLUMN)
   private String identificationNumber;
 
@@ -65,17 +67,29 @@ public class Problem {
   private State state;
   private String reporter;
 
-  /** This enum describes the possible states of a problem. */
+  /**
+   * This enum describes the possible states of a problem.
+   */
   public enum State {
-    /** Open state. */
+    /**
+     * Open state.
+     */
     OPEN,
-    /** Accepted state. */
+    /**
+     * Accepted state.
+     */
     ACCEPTED,
-    /** In progress state. */
+    /**
+     * In progress state.
+     */
     IN_PROGRESS,
-    /** Declined state. */
+    /**
+     * Declined state.
+     */
     DECLINED,
-    /** Closed state. */
+    /**
+     * Closed state.
+     */
     CLOSED
   }
 }
