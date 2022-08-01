b22a79c54e25733f51841dbc89f96674b17e6913
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-08-01 17:40:29 +0200

    checkstyle v2

--- src/main/java/edu/kit/tm/cm/smartcampus/problem/api/controller/dto/ServerCreateProblemRequest.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/problem/api/controller/dto/ServerCreateProblemRequest.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * This class represents a request for a Problem.
+ * This class represents a create request for a Problem.
  */
 @Data
 @Setter
--- src/main/java/edu/kit/tm/cm/smartcampus/problem/api/controller/dto/ServerUpdateProblemRequest.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/problem/api/controller/dto/ServerUpdateProblemRequest.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * This class represents an update request for a Problem.
+ */
 @Setter
 @Getter
 @AllArgsConstructor
